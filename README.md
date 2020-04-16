09b880b6f7956fd2a4b92daa38de899892ab3f4a
rjflatley <60044608+rjflatley@users.noreply.github.com>
2020-04-15 23:54:21 -0400

    Moved to Wifi TCP logging on ESP8266 D1 Mini

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # vat1300
 Arduino nRF24L01+ data logger for Juntek VAT-1300 wireless battery monitor
+
+Tested on Arduino ESP8266 (LOLIN(WEMOS) D1 R2 & mini)
+
+//Change the following lines to match your setup:
+const char* ssid = "change this to your Wifi access point SSID";
+const char* password = "change this to your Wifi access point password";
+
+//Change the following to match the address and frequency as set on the Juntek controller
+const int address = 17;
+const char frequency = 'A';
+
+
+Watch the following YouTube videos for more information
+https://www.youtube.com/watch?v=DNORE1HBbRo
+https://www.youtube.com/watch?v=bQKVNZRaFuQ&t=1s
+https://www.youtube.com/watch?v=S0-O9cTS9Uw
--- vat1300_rf24_data_logger.ino
+++ vat1300_rf24_data_logger.ino
@@ -1,39 +1,63 @@
-/*  Juntek VAT-1300 data logger   
- *  tested on Arduino Uno and ESP32 (Node32s)
- */
+/*  Juntek VAT-1300 data logger
+    tested on Arduino ESP8266 (LOLIN(WEMOS) D1 R2 & mini)
+*/
+#include <ESP8266WiFi.h>
 #include <SPI.h>
 #include "RF24.h"
 #include "printf.h"
 
 
 #if defined(ARDUINO_AVR_UNO)
-  // Uno pin assignments
-  const int irqPin = 2;
-  const int cePin = 7;
-  const int csnPin = 8;
-  const int mosiPin = 11;
-  const int misoPin = 12;
-  const int sckPin = 13;
+// Uno pin assignments
+const int irqPin = 2;
+const int cePin = 7;
+const int csnPin = 8;
+const int mosiPin = 11;
+const int misoPin = 12;
+const int sckPin = 13;
 //#elif defined(ARDUINO_SAM_DUE)
 //  // Due assignments
-//  // SPI pins csn, mosi, miso, sck are wired directly to SPI header 
-//  #define RF24_DUE 
+//  // SPI pins csn, mosi, miso, sck are wired directly to SPI header
+//  #define RF24_DUE
 //  const int irqPin = 6;
 //  const int cePin = 7;
 //  const int csnPin = 10;
 #elif defined(ARDUINO_ARCH_ESP32)
-  const int irqPin = 4;
-  const int cePin = 17;
-  const int csnPin = 5;
-  const int mosiPin = 23;
-  const int misoPin = 19;
-  const int sckPin = 18;
+const int irqPin = 4;
+const int cePin = 17;
+const int csnPin = 5;
+const int mosiPin = 23;
+const int misoPin = 19;
+const int sckPin = 18;
+#elif defined(ARDUINO_ARCH_ESP8266)
+const int irqPin = D3;
+const int cePin = D2;
+const int csnPin = D8;
+const int mosiPin = D6;
+const int misoPin = D7;
+const int sckPin = D5;
 #else
-  #error Unsupported board selection.
+#error Unsupported board selection.
 #endif
 
-const int address = 27;
+//how many clients should be able to telnet to this ESP8266
+#define MAX_SRV_CLIENTS 2
+//Change the following lines to match your setup:
+const char* ssid = "change this to your Wifi access point SSID";
+const char* password = "change this to your Wifi access point password";
+
+WiFiServer server(23);
+WiFiClient serverClients[MAX_SRV_CLIENTS];
+
+unsigned long previousMillis = 0;
+const long interval = 60000;  //output every 60 seconds
+
+//Change the following to match the address and frequency as set on the Juntek controller
+const int address = 17;
 const char frequency = 'A';
+
+float battAmpHours = -2.0;
+
 
 /****************** User Config ***************************/
 /***      Set this radio as radio number 0 or 1         ***/
@@ -80,11 +104,26 @@ void setup() {
   printf_begin();
 
   configureRadio();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  uint8_t i = 0;
+  while (WiFi.status() != WL_CONNECTED && i++ < 20) delay(500);
+  if (i == 21) {
+    Serial.print("Could not connect to"); Serial.println(ssid);
+    while (1) delay(500);
+  }
+  server.begin();
+  server.setNoDelay(true);
+
+
+
 }
 
 uint32_t configTimer =  millis();
 
 void loop() {
+
 
   if (radio.failureDetected) {
     radio.failureDetected = false;
@@ -105,25 +144,49 @@ void loop() {
 
   radio.stopListening();                                    // First, stop listening so we can talk.
 
+  //***** wifi stuff  *******
+
+  unsigned long currentMillis = millis();
+  uint8_t i;
+  //check if there are any new clients
+  if (server.hasClient()) {
+    for (i = 0; i < MAX_SRV_CLIENTS; i++) {
+      //find free/disconnected spot
+      if (!serverClients[i] || !serverClients[i].connected()) {
+        if (serverClients[i]) serverClients[i].stop();
+        serverClients[i] = server.available();
+        continue;
+      }
+    }
+    //no free/disconnected spot so reject
+    WiFiClient serverClient = server.available();
+    serverClient.stop();
+  }
+
+  //***** end wifi stuff ******
+
+
+
+
 
   //Serial.println(F("Now sending"));
-  byte aTX_PAYLOAD[] = { 0xAA, 0x04, 0x02, address, address + 0xB0, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00 }; 
+  byte aTX_PAYLOAD[] = { 0xAA, 0x04, 0x02, address, address + 0xB0, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00 };
 
 
   aTX_PAYLOAD[4] = address + 0xB0;
-/* first time in loop set TX_Payload to fetch setup data */
+  /* first time in loop set TX_Payload to fetch setup data */
   if (lpCnt == 0)
   {
     aTX_PAYLOAD[2] = 2;
     aTX_PAYLOAD[4] = address + 0xB0;
   }
-/* second time onward in loop set TX_Payload to fetch run data */
+  /* second time onward in loop set TX_Payload to fetch run data */
   if (lpCnt == 1)
   {
     aTX_PAYLOAD[2] = 1;
     aTX_PAYLOAD[4] = address + 0xAF;
   }
-/* special recovery of base unit for power failures */
+  /* special recovery of base unit for power failures */
   if (lpCnt < 0)
   {
     aTX_PAYLOAD[2] = 0xFF;
@@ -168,25 +231,13 @@ void loop() {
     }
     unsigned long end_time = micros();
 
-    // Spew it
-    /*Serial.print(F("Sent "));
-      Serial.print(start_time);
-      Serial.print(F(", Got response "));
-      Serial.print(got_time);
-      Serial.print(F(", Round-trip delay "));
-      Serial.print(end_time - start_time);
-      Serial.println(F(" microseconds"));*/
-    /*for (int i = 0; i < 32; i++)
-      {
-      Serial.print(aRX_PAYLOAD[i], HEX);
-      Serial.print(",");
-      }
-      Serial.println("");*/
-      
-/* Print headers and setup data  */
+
+
+    /* Print headers and setup data  */
     if (lpCnt == 0)
     {
-      float battAmpHours  = (uint16_t)(aRX_PAYLOAD[6] << 8 | aRX_PAYLOAD[7]) / 10.0;
+      if (battAmpHours < -1.0)
+        battAmpHours  = (uint16_t)(aRX_PAYLOAD[6] << 8 | aRX_PAYLOAD[7]) / 10.0;
       float ovp           = (int16_t)(aRX_PAYLOAD[8]  << 8 | aRX_PAYLOAD[9]) / 100.0;
       float lvp           = (int16_t)(aRX_PAYLOAD[10] << 8 | aRX_PAYLOAD[11]) / 100.0;
       float ocp           = (int16_t)(aRX_PAYLOAD[12] << 8 | aRX_PAYLOAD[13]);
@@ -215,47 +266,75 @@ void loop() {
       Serial.print(calCurrentIeq0);
       Serial.print("\t");
       Serial.println(calCurrentIgt0);
-      Serial.println("\t\t\tAmp\tWatt\tTime\t\t");
-      Serial.println("Voltage\tCurrent\tWatts\thours\thours\tseconds\tTemp\tOutput");
+      //Serial.println("\t\t\tAmp\tWatt\tTime\t\t");
+      //Serial.println("Voltage\tCurrent\tWatts\thours\thours\tseconds\tTemp\tOutput");
     }
     else
-/* print run data */    
+      /* print run data */
     {
-      float voltage   = (int16_t)(aRX_PAYLOAD[4]  << 8  | aRX_PAYLOAD[5]) / 100.0;
-      float amperage  = (int16_t)(aRX_PAYLOAD[6]  << 8  | aRX_PAYLOAD[7]) / 10.0;
-      float wattage   = (int32_t)(aRX_PAYLOAD[8]  << 24 | aRX_PAYLOAD[9]  << 16 | aRX_PAYLOAD[10] << 8 |  aRX_PAYLOAD[11]) / 1000.0;
-      float ampHours  = (int32_t)(aRX_PAYLOAD[12] << 24 | aRX_PAYLOAD[13] << 16 | aRX_PAYLOAD[14] < 8 |  aRX_PAYLOAD[15])  / 1000.0;
-      float wattHours = (int32_t)(aRX_PAYLOAD[16] << 24 | aRX_PAYLOAD[17] << 16 | aRX_PAYLOAD[18] << 8 | aRX_PAYLOAD[19])  / 1000.0;
-      int32_t seconds = (int32_t)(aRX_PAYLOAD[20] << 24 + aRX_PAYLOAD[21] << 16 | aRX_PAYLOAD[22] << 8 | aRX_PAYLOAD[23]);
-      int8_t temperature = (int8_t)aRX_PAYLOAD[25];
-      bool outputOn;
-      if (aRX_PAYLOAD[24] == 0)
-        outputOn = true;
-      else
-        outputOn = false;
 
-      Serial.print(voltage, 2);
-      Serial.print("\t");
-      Serial.print(amperage, 1);
-      Serial.print("\t");
-      Serial.print(wattage, 3);
-      Serial.print("\t");
-      Serial.print(ampHours, 3);
-      Serial.print("\t");
-      Serial.print(wattHours, 3);
-      Serial.print("\t");
-      Serial.print(seconds);
-      Serial.print("\t");
-      Serial.print(temperature);
-      Serial.print("\t");
-      if (outputOn == true)
-        Serial.println("On ");
-      else
-        Serial.println("Off");
+      //***** more wifi stuff *****
+
+      //do every 1000ms
+      if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+
+        //*****  end more wifi stuff
+
+        float voltage   = (int16_t)(aRX_PAYLOAD[4]  << 8  | aRX_PAYLOAD[5]) / 100.0;
+        float amperage  = (int16_t)(aRX_PAYLOAD[6]  << 8  | aRX_PAYLOAD[7]) / 10.0;
+        float wattage   = (int32_t)(aRX_PAYLOAD[8]  << 24 | aRX_PAYLOAD[9]  << 16 | aRX_PAYLOAD[10] << 8 |  aRX_PAYLOAD[11]) / 1000.0;
+        float ampHours  = (int32_t)(aRX_PAYLOAD[12] << 24 | aRX_PAYLOAD[13] << 16 | aRX_PAYLOAD[14] << 8 |  aRX_PAYLOAD[15])  / 1000.0;
+        float wattHours = (int32_t)(aRX_PAYLOAD[16] << 24 | aRX_PAYLOAD[17] << 16 | aRX_PAYLOAD[18] << 8 | aRX_PAYLOAD[19])  / 1000.0;
+        int32_t seconds = (int32_t)(aRX_PAYLOAD[20] << 24 | aRX_PAYLOAD[21] << 16 | aRX_PAYLOAD[22] << 8 | aRX_PAYLOAD[23]);
+        int8_t temperature = (int8_t)aRX_PAYLOAD[25];
+        float batterySOC = (battAmpHours - ampHours) / battAmpHours * 100.0;
+
+        bool outputOn;
+        if (aRX_PAYLOAD[24] == 0)
+          outputOn = true;
+        else
+          outputOn = false;
+
+
+        int isensorValue = temperature;
+        String sSensorValue = String("SOC BatteryVoltage=") + String(voltage, 2)
+                              + String(",LoadCurrent=")         + String(amperage, 1)
+                              + String(",LoadPower=")           + String(wattage, 3)
+                              + String(",BatteryTemperature=")  + String(temperature)
+                              + String(",EquipmentStatus=")      + String(outputOn)
+                              + String(",AmpHours=")            + String(ampHours, 3)
+                              + String(",WattHours=")           + String(wattHours, 3)
+                              + String(",OnTime=")              + String(seconds)
+                              + String(",BatterySOC=")          + String(batterySOC, 5) + "\n\r";
+        /*        String sSensorValue = String("BatteryVoltage value=") + String(voltage, 2) + "\n\r"
+                                      + String("LoadCurrent value=")         + String(amperage, 1)  + "\n\r"
+                                      + String("LoadPower value=")           + String(wattage, 3)  + "\n\r"
+                                      + String("BatteryTemperature value=")  + String(temperature)  + "\n\r"
+                                      + String("EquipmentStatus vaue=")      + String(outputOn)  + "\n\r"
+                                      + String("AmpHours value=")            + String(ampHours, 3)  + "\n\r"
+                                      + String("WattHours value=")           + String(wattHours, 3) + "\n\r"
+                                      + String("OnTime value=")              + String(seconds) + "\n\r"
+                                      + String("BatterySOC value=")          + String(batterySOC) + "\n\r";*/
+        Serial.print(sSensorValue);
+        size_t len = sSensorValue.length();
+        uint8_t sbuf[len];
+        sSensorValue.getBytes(sbuf, len);
+        for (i = 0; i < MAX_SRV_CLIENTS; i++) {
+          if (serverClients[i] && serverClients[i].connected()) {
+            serverClients[i].write(sbuf, len);
+            delay(1);
+          }
+        }
+
+
+
+      }
     }
     lpCnt = 1;
   }
 
-  //    delay(1000);
+  //  }
+  //      delay(1000);
 
 } // Loop
